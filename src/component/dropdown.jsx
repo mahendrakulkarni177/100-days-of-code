84aea06e954a37cb994b2cb919a2649c068a111b
Mahendra Kulkarni <mkraj177@gmail.com>
2019-12-01 13:42:10 +0530

    new compoent directory

---
+++ src/component/dropdown.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class DropDown extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+
+    render() {
+        return (
+            <div>DropDown</div>
+        );
+    }
+}
+
+export default DropDown;
